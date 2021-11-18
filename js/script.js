17dad066688da00167a5bfd9d114dca9333f2a94
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-18 16:54:16 -0300

    feito sessao trabalhe conosco
    nessa sessao usamos grid e parallax juntos

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ $(document).ready(function(){
 setTimeout(function(){
 
     $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
-
+    $('#apply-area').parallax({imageSrc: 'img/pattern.png'})
 }, 250)
 
 })
