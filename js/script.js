3b0737f1b8a094a70fc480c6d3d24c69580c0d16
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-21 15:06:39 -0300

    projeto finalizado - algumas coisas precisam ser refeitas no css

--- js/script.js
+++ js/script.js
@@ -118,4 +118,40 @@ $('.filter-btn').on('click', function(){
      }
  }
 
+ //scroll para as sessões
+ let navBtn =  $('.nav-item');
+
+ let bannerSection = $('#mainSlider');
+ let aboutSection = $('#about-area');
+ let servicesSection = $('#services-area');
+ let teamSection = $('#team-area');
+ let portfolioSection = $('#portfolio-area');
+ let contactSection = $('#contact-area');
+
+ let scrollTo = ''
+ $(navBtn).click(function(){
+   let btnId = $(this).attr('id')
+   console.log(btnId)
+
+   if(btnId == 'about-menu'){
+     scrollTo = aboutSection
+   }else if(btnId == 'services-menu'){
+     scrollTo = servicesSection
+   }else if(btnId == 'team-menu'){
+     scrollTo = teamSection
+   }else if(btnId == 'portfolio-menu'){
+     scrollTo = portfolioSection
+   }else if(btnId = 'contact-menu'){
+     scrollTo = contactSection
+   }else if(btnId == 'home-menu'){
+     scrollTo = bannerSection
+   }
+
+   $([document.documentElement, document.body]).animate({
+    scrollTop: $(scrollTo).offset().top - 70
+  }, 1500)
+
+ })
+ 
+
 })
